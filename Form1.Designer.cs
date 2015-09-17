4539c7a07d3feb2743a6a116a5106fbcb8d90bd0
corerman <admin@icorer.com>
2015-09-17 19:24:37 +0800

    First Upload Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CodeJudge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.skinProgressBar1 = new CCWin.SkinControl.SkinProgressBar();
+            this.skinButton1 = new CCWin.SkinControl.SkinButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // skinProgressBar1
+            // 
+            this.skinProgressBar1.Back = null;
+            this.skinProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.skinProgressBar1.BarBack = null;
+            this.skinProgressBar1.BarRadiusStyle = CCWin.SkinClass.RoundStyle.All;
+            this.skinProgressBar1.ForeColor = System.Drawing.Color.White;
+            this.skinProgressBar1.Location = new System.Drawing.Point(7, 96);
+            this.skinProgressBar1.Name = "skinProgressBar1";
+            this.skinProgressBar1.RadiusStyle = CCWin.SkinClass.RoundStyle.All;
+            this.skinProgressBar1.Size = new System.Drawing.Size(330, 23);
+            this.skinProgressBar1.TabIndex = 0;
+            this.skinProgressBar1.TrackBack = System.Drawing.Color.Gray;
+            this.skinProgressBar1.TrackFore = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            // 
+            // skinButton1
+            // 
+            this.skinButton1.BackColor = System.Drawing.Color.Transparent;
+            this.skinButton1.BaseColor = System.Drawing.Color.Peru;
+            this.skinButton1.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.skinButton1.DownBack = null;
+            this.skinButton1.Location = new System.Drawing.Point(127, 47);
+            this.skinButton1.MouseBack = null;
+            this.skinButton1.Name = "skinButton1";
+            this.skinButton1.NormlBack = null;
+            this.skinButton1.Size = new System.Drawing.Size(84, 33);
+            this.skinButton1.TabIndex = 1;
+            this.skinButton1.Text = "选择文件";
+            this.skinButton1.UseVisualStyleBackColor = false;
+            this.skinButton1.Click += new System.EventHandler(this.skinButton1_Click);
+            this.skinButton1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouse_on);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.CanResize = false;
+            this.CaptionFont = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ClientSize = new System.Drawing.Size(344, 135);
+            this.Controls.Add(this.skinButton1);
+            this.Controls.Add(this.skinProgressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Radius = 10;
+            this.SkinOpacity = 0.9D;
+            this.Text = "文件云传送  [CorerMan]";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.form_mouse_on);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CCWin.SkinControl.SkinProgressBar skinProgressBar1;
+        private CCWin.SkinControl.SkinButton skinButton1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using CCWin;
+using System.Net;
+using System.Diagnostics;
+
+namespace CodeJudge
+{
+    public partial class Form1 : Skin_Mac
+    {
+        Stopwatch sw = new Stopwatch();
+        TimeSpan ts;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void wc_UploadProgressChanged(object sender, UploadProgressChangedEventArgs e)
+        {
+            skinProgressBar1.Maximum = (int)e.TotalBytesToSend;
+            skinProgressBar1.Value = (int)e.BytesSent;
+            ts = sw.Elapsed;
+            double SendDataSize = (double)e.BytesSent;
+            double speed=SendDataSize/ts.TotalMilliseconds; //KB/S
+            skinButton1.Text = speed.ToString("0.00 KB/S");
+        }
+
+
+        private void skinButton1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = ""; //对话框初始化
+            openFileDialog1.ShowDialog();//显示对话框
+            String total_filename=openFileDialog1.FileName;
+            String short_filename=openFileDialog1.SafeFileName;
+            //MessageBox.Show(total_filename);
+            if (short_filename.ToString() != "")
+            {
+                String keyword = Microsoft.VisualBasic.Interaction.InputBox("请输入口令:", "安全验证"); //对输入的口令进行加密运算
+                string md5_password = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(keyword.ToString(), "MD5");
+                //MessageBox.Show(password);
+                if (md5_password.ToString() == "16F09AE0A377EDE6206277FAD599F9A0")
+                {
+
+                    String url_link = "ftp://clouduser:" + keyword.ToString() + "@133.130.89.177/" + short_filename;
+                    WebClient wc = new WebClient();
+                    wc.UploadProgressChanged += new UploadProgressChangedEventHandler(wc_UploadProgressChanged);
+                    wc.UploadFileCompleted += new UploadFileCompletedEventHandler(wc_UploadFileCompleted);
+                    wc.UploadFileAsync(new Uri(url_link), total_filename);
+                    skinButton1.Enabled = false;
+                    skinButton1.ForeColor = System.Drawing.Color.Black;
+                    //计算用时，计算上传速度
+                    sw.Reset();
+                    sw.Start();
+
+
+                }
+                else
+                    MessageBox.Show("口令验证失败！");
+            }
+        }
+
+        void wc_UploadFileCompleted(object sender, UploadFileCompletedEventArgs e)
+        {
+            skinButton1.Enabled = true;
+            sw.Stop();
+            skinButton1.Text = "选择文件";
+            skinProgressBar1.Value = 0;
+        }
+
+        private void mouse_on(object sender, MouseEventArgs e)
+        {
+            skinButton1.BaseColor =System.Drawing.Color.DarkRed;
+            skinButton1.ForeColor = System.Drawing.Color.White;
+        }
+
+        private void form_mouse_on(object sender, MouseEventArgs e)
+        {
+            skinButton1.BaseColor = System.Drawing.Color.Peru;
+            skinButton1.ForeColor = System.Drawing.Color.Black;
+        }
+    }
+}
